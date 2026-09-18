d112b46fe3ffa4e523cf5521b11005519f6b16fb
Shi Ting <2600663@sit.singaporetech.edu.sg>
2026-09-18 15:48:32 +0800

    Everything is done

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,4 +1,4 @@
-def get_valid_input(stock):
+def get_valid_input():
     stock = input("Enter stock quantity: ")
     if stock!= "quit":
         if stock.isdigit():
@@ -6,8 +6,10 @@ def get_valid_input(stock):
            return int(stock)
         elif stock.indigit()<0:
             print("No negative numbers")
+            return None
         else:
-           print("The stock quantity entered is not a valid integer")      
+            print("The stock quantity entered is not a valid integer")      
+            return None
     else:
         return stock
 
@@ -19,30 +21,31 @@ def calculate_tax(amount):
     tax = 0.10 * amount
     return tax
 
+def generate_report(total_units, failed_attempts):
+    print ("Total Deliveries Processed:", total_units)
+    print("Failed_attempts", failed_attempts )
 
 inventory = 0
 reject = 0
 stock = ""
 
-while stock!= "quit":
-    stock = input("Enter stock quantity: ")
-    if stock.isdigit():
-        stock = int(stock)
-        if stock < 0:
-            print("negative number not allowed")
-            reject = reject + 1
-            continue
-        inventory = inventory + stock
-        print("Stock added")
-        if inventory >= 500:
-            print("Inventory is already 500 or more")
-            break
-    else:
-        print("Integers or quit only")
+while True:
+    stock = get_valid_input()
+    if stock == "quit":
+        break
+    if stock == None:
         reject = reject + 1
+        continue
+    inventory = process_delivery(inventory, stock)
+    print("Stock added", stock)
+    tax_amount = calculate_tax(stock)
+    print("Tax amount(10%): ", tax_amount)
+    if inventory > 500:
+        print("Overstock alert: the inventory has exceeded 500 units" , inventory)
+        break
+    
+generate_report(inventory,reject)
 
-print("Total Units Processed: ", inventory)
-print("Number of Failed Entries: ", reject )
 
 
 
